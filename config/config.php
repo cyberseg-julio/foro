68195f221533429630f97db386bc0766d441b00c
cyberseg-julio <julioalberto.amaya.alu@iescampanillas.com>
2021-04-21 21:46:59 +0200

    Version Inicial

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+//DB Params
+define ("DB_HOST", "mysqldb");
+define ("DB_USER", "dev");
+define ("DB_PASS", "dev");
+define ("DB_NAME", "forum");
+
+define("SITE_TITLE", "Bienvenido al foro!");
+
+//Paths
+define('BASE_URI','http://prodseg:8000/foro/');
