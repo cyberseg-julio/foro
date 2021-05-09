16520d7b7dfc40a95c18371431ca9662d6a5e355
cyberseg-julio <julioalberto.amaya.alu@iescampanillas.com>
2021-05-09 20:00:05 +0200

    Captcha refresh

--- captcha.php
+++ captcha.php
@@ -1,9 +1,5 @@
 <?php
 session_start();
-header("Expires: Mon, 31 Jul 2021 05:00:00 GMT");
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-header("Cache-Control: no-store, no-cache, must-revalidate");
-header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 function _generateRandom($length=6)
 {
